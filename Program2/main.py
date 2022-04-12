26080859b7c5680417302f669b74a55a72050912
HaydenCenter <haydencenter72@gmail.com>
2022-04-11 22:10:30 -0500

    Submission

--- Program2/main.py
+++ Program2/main.py
@@ -1,6 +1,6 @@
 from PLSI import PLSI
 
-plsi = PLSI(dirName="Program2/_test", randomInit="dirichlet", topicCount=3, iterations=2)
+plsi = PLSI(dirName="Program2/_test", randomInit="", topicCount=3, iterations=2, toLoad=True)
 print(plsi.getDocumentTopic(docName="a.txt"))
 [print(x, y) for x, y in plsi.getAllDocumentTopic()]
 print(plsi.getTopicWordVector(0, topCount=1))
