555477d7225626fa5f0884363e606ca723de663a
HaydenCenter <haydencenter72@gmail.com>
2022-05-06 15:36:08 -0500

    Moving files

--- Project/pdf.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-reader = PyPDF2.PdfFileReader("94.pdf")
-numPages = reader.numPages
-text = []
-for page in range(numPages):
-    text.append(reader.pages[page].extractText())
-
-print("\n".join(text))
